cd4a2a11d16d06da6ca9f747ab40872eefe8baee
Ian Astalosh <iastalosh@gmail.com>
2021-02-22 11:12:21 +1100

    Add configs and utils

---
+++ configs.R
@@ -0,0 +1,20 @@
+
+# Configs
+
+# Data files
+WOMENS_DATA_STRING = 'data/hackathon_womens.csv'
+NWHL_DATA_STRING = 'data/hackathon_nwhl.csv'
+
+# Ice dimensions
+X_MAX = 200
+Y_MAX = 85
+
+GOAL_LINE_1_X = 11
+BLUE_LINE_1_X = 11 + 64
+CENTRE_X = 100
+BLUE_LINE_2_X = 11 + 64 + 50
+GOAL_LINE_2_X = 11 + 64 + 50 + 64
+
+# Determine what x value to draw edges of rink at
+CURVE_START_X_1 = 28
+CURVE_START_X_2 = X_MAX - 28
---
+++ utils.R
@@ -0,0 +1,5 @@
+
+# Utils
+
+# Create 'not in' function
+'%!in%' <- function(x,y)!('%in%'(x,y))
